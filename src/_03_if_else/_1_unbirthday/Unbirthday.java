3401cda3ceea440e2d36e6d29a5bf3289bfde8f0
league <league@imac-20.attlocal.net>
2022-07-02 16:33:54 -0700

    venn diagram, unbirthday, and remarkable

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,22 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		String yesorno = JOptionPane.showInputDialog("Is today your birthday? Yes or no?"); 
+		if (yesorno.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null,"Congradulations!"); 
+		}
+			
+		if (yesorno.equalsIgnoreCase( "no")) {
+				JOptionPane.showMessageDialog(null,"Oh, that's too bad, well continue what you were doing."); 
+		}
+		
+	}
+
+	
+
+}
+	
+
