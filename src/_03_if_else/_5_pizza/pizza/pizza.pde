3b763d300f99de8a2f07a6aec0c7caaa92f77e2b
Your Name <you@example.com>
2022-07-16 16:33:08 -0700

    pizza, riddler, and robot in space

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -13,8 +13,19 @@ void draw() {
 }
   fill(255,255,0);
   ellipse(443, 434, 400, 400);
-  image(pepperoni,309,285);
-  image(pepperoni,246,435);
+  image(pepperoni,288,480);
+  image(pepperoni,419,282);
+  image(pepperoni,540,361);
+  image(mushroom,498,296);
+  image(mushroom,347,327);
+  image(pepperoni,265,313);
+  image(mushroom,252,409);
+  image(mushroom, 380,537);
+  image(pepperoni, 469,507);
+  image(mushroom, 514, 437);
+  image(pepperoni, 330,240);
+  image(pepperoni, 373,447);
+  image(mushroom, 422, 379);
   
 }
  
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,23 +2,53 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+            int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+              String riddle = JOptionPane.showInputDialog("What word starts with E and ends with E but only has one letter in it?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+              if (riddle.equalsIgnoreCase("envelope")) {
+            	  JOptionPane.showMessageDialog(null,"Correct! Great job!"); 
+            	  score++;
+              }
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+              else {
+            			JOptionPane.showMessageDialog(null,"Wrong.  the anwser is envelope");
+            		}
 		// 6. Add some more riddles
-
+              riddle = JOptionPane.showInputDialog("What is full of holes but still holds a lot of water?");
+              if (riddle.equalsIgnoreCase("sponge")) {
+            	  JOptionPane.showMessageDialog(null,"Correct! Great job!"); 
+            	  score++;
+              }
+              else {
+      			JOptionPane.showMessageDialog(null,"Wrong.  the anwser is a sponge");
+      		}
+             riddle = JOptionPane.showInputDialog("What word looks the same upside down and backward?");
+              if (riddle.equalsIgnoreCase("swims")) {
+            	  JOptionPane.showMessageDialog(null,"Correct! Great job!"); 
+            	  score++;
+              }
+              else {
+      			JOptionPane.showMessageDialog(null,"Wrong.  the anwser is 'swims' .");
+      		}
+              riddle = JOptionPane.showInputDialog("Give me food, and I will live. Give me water, and I will die. What am I?");
+              if (riddle.equalsIgnoreCase("swims")) {
+            	  JOptionPane.showMessageDialog(null,"Correct! Great job!"); 
+            	  score++;
+              }
+              else {
+      			JOptionPane.showMessageDialog(null,"Wrong.  the anwser is fire .");
+      		}
+              
 		// 2. Make a pop up to show the score.
+              JOptionPane.showMessageDialog(null,"Your score is "+score);
 		
 	}
 }
